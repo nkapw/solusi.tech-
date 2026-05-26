8f0ae56d667a1ded2b59368b7a4110686e13895e
Imam Ahmad Fahrezi <imamfahrezi24@gmail.com>
2026-05-26 19:49:00 +0700

    feat: scaffold Astro project with sitemap and tailwind

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  site: 'https://solusi.id',
+  integrations: [
+    sitemap(),
+    tailwind({ applyBaseStyles: false }),
+  ],
+});
